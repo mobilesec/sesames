8b1a92b406e6c8bb9e06f24b6c6c6f0584493818
Ulrich Lehner <me@ulrichlehner.com>
2012-02-21 20:00:50 +0100

    Beautified comparision charts. Graphic changes in computer room list.

--- MID_SesameDisplay/src/at/sesame/fhooe/midsd/hd/HD_Comparison_Bar_RendererProvider.java
+++ MID_SesameDisplay/src/at/sesame/fhooe/midsd/hd/HD_Comparison_Bar_RendererProvider.java
@@ -7,11 +7,31 @@ import at.sesame.fhooe.lib.ui.charts.AbstractRendererProvider;
 
 public class HD_Comparison_Bar_RendererProvider extends
 		AbstractRendererProvider {
+	
+	private int mFillColorAlpha = 180;
 
 	@Override
 	public XYSeriesRenderer setupSeriesRenderer(XYSeries arg0) {
 		// TODO Auto-generated method stub
 		return new XYSeriesRenderer();
+	}
+	
+	@Override
+	protected void setupRenderer() {
+		super.setupRenderer();
+		mRenderer.setShowGrid(false);
+		mRenderer.setApplyBackgroundColor(false);
+		mRenderer.setMarginsColor(0x00ffffff);
+		//mRenderer.setMargins(new int[] { 0, 100, 70, 100 });
+		mRenderer.setAxesColor(0xffffffff);
+		mRenderer.setLabelsColor(0xffffffff);
+		//mRenderer.setLabelsTextSize(20);
+		mRenderer.setClickEnabled(false);
+		//mRenderer.setLegendTextSize(50);
+		//mRenderer.setLegendHeight(70);
+		mRenderer.setPanEnabled(false, false);
+		mRenderer.setZoomButtonsVisible(false);
+		mRenderer.setZoomEnabled(false, false);
 	}
 
 }
--- MID_SesameDisplay/src/at/sesame/fhooe/midsd/hd/HD_Comparison_RendererProvider.java
+++ MID_SesameDisplay/src/at/sesame/fhooe/midsd/hd/HD_Comparison_RendererProvider.java
@@ -8,40 +8,52 @@ import at.sesame.fhooe.lib.ui.charts.AbstractRendererProvider;
 
 public class HD_Comparison_RendererProvider extends AbstractRendererProvider {
 
+	private int mFillColorAlpha = 180;
+
 	@Override
 	public XYSeriesRenderer setupSeriesRenderer(XYSeries arg0) {
-		// TODO Auto-generated method stub
 		XYSeriesRenderer xysr = new XYSeriesRenderer();
-		xysr.setColor(getColorForSeries(arg0));
+		int color = getColorForSeries(arg0);
+		xysr.setColor(color);
+		xysr.setLineWidth(3.0f);
+		xysr.setFillBelowLine(true);
+		xysr.setFillBelowLineColor(Color.argb(mFillColorAlpha,
+				Color.red(color), Color.green(color), Color.blue(color)));
 		return xysr;
 	}
 
-	private int getColorForSeries(XYSeries _series)
-	{
+	private int getColorForSeries(XYSeries _series) {
 		String title = _series.getTitle();
-		if(title.contains(ComparisonFragment.CURRENT_DATA_NAME))
-		{
+		if (title.contains(ComparisonFragment.CURRENT_DATA_NAME)) {
 			return Color.BLUE;
-		}
-		else if(title.contains(ComparisonFragment.DAY_CB1_TEXT))
-		{
+		} else if (title.contains(ComparisonFragment.DAY_CB1_TEXT)) {
 			return Color.RED;
-		}
-		else if(title.contains(ComparisonFragment.DAY_CB2_TEXT))
-		{
+		} else if (title.contains(ComparisonFragment.DAY_CB2_TEXT)) {
 			return Color.GREEN;
-		}
-		else if(title.contains(ComparisonFragment.DAY_CB3_TEXT))
-		{
+		} else if (title.contains(ComparisonFragment.DAY_CB3_TEXT)) {
 			return Color.YELLOW;
-		}
-		else if(title.contains(ComparisonFragment.DAY_CB4_TEXT))
-		{
+		} else if (title.contains(ComparisonFragment.DAY_CB4_TEXT)) {
 			return Color.LTGRAY;
-		}
-		else
-		{
+		} else {
 			return Color.BLACK;
 		}
+	}
+
+	@Override
+	protected void setupRenderer() {
+		super.setupRenderer();
+		mRenderer.setShowGrid(false);
+		mRenderer.setApplyBackgroundColor(false);
+		mRenderer.setMarginsColor(0x00ffffff);
+		// mRenderer.setMargins(new int[] { 0, 100, 70, 100 });
+		mRenderer.setAxesColor(0xffffffff);
+		mRenderer.setLabelsColor(0xffffffff);
+		// mRenderer.setLabelsTextSize(20);
+		mRenderer.setClickEnabled(false);
+		// mRenderer.setLegendTextSize(50);
+		// mRenderer.setLegendHeight(70);
+		mRenderer.setPanEnabled(true, true);
+		mRenderer.setZoomButtonsVisible(false);
+		mRenderer.setZoomEnabled(true, true);
 	}
 }
