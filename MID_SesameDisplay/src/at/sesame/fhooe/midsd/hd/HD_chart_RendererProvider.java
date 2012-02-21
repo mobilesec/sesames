d4455dd50950ac1e98d3e4799b02ef8c18a43371
Peter Riedl <peter.riedl@fh-hagenberg.at>
2012-02-21 14:07:40 +0100

    HD renderer provider updated to have different colors

--- MID_SesameDisplay/src/at/sesame/fhooe/midsd/hd/HD_chart_RendererProvider.java
+++ MID_SesameDisplay/src/at/sesame/fhooe/midsd/hd/HD_chart_RendererProvider.java
@@ -3,16 +3,41 @@ package at.sesame.fhooe.midsd.hd;
 import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYSeriesRenderer;
 
+import android.graphics.Color;
 import at.sesame.fhooe.lib.ui.charts.AbstractRendererProvider;
 
 public class HD_chart_RendererProvider
 extends AbstractRendererProvider
 {
-
+	private static final String EDV1_TITLE = "EDV 1";
+	private static final String EDV3_TITLE = "EDV 3";
+	private static final String EDV6_TITLE = "EDV 6";
 	@Override
 	public XYSeriesRenderer setupSeriesRenderer(XYSeries arg0) {
 		// TODO Auto-generated method stub
-		return new XYSeriesRenderer();
+		XYSeriesRenderer xysr = new XYSeriesRenderer();
+		xysr.setColor(getColorForSeries(arg0));
+		return xysr;
+	}
+	
+	private int getColorForSeries(XYSeries _series)
+	{
+		if(_series.getTitle().equals(EDV1_TITLE))
+		{
+			return Color.GREEN;
+		}
+		else if(_series.getTitle().equals(EDV3_TITLE))
+		{
+			return Color.RED;
+		}
+		else if(_series.getTitle().equals(EDV6_TITLE))
+		{
+			return Color.YELLOW;
+		}
+		else
+		{
+			return Color.BLUE;
+		}
 	}
 
 }
